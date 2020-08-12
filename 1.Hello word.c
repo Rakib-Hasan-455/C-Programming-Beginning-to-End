57464aa4c7674cf46b5e5377c91d1c2c3fbb0bd3
Md. Rakibul Hasan <hasanrakib455@gmail.com>
2020-08-12 22:23:52 +0000

    1 --Hello world.

---
+++ 1.Hello word.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(void){
+printf("Hello world");
+return 0;
+}
+/* #include<stdio.h>
+int main(){
+printf("Hello world");
+return 0;
+}
+
+#include<stdio.h>
+void main(){
+printf("Hello world");
+return 0;
+}
+
+#include<stdio.h>
+main(){
+printf("Hello world");
+}
+*/
