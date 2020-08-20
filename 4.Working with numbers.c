b89d65ed8404ca7d61c26f6e7b6ce8aa42b16d22
Md. Rakibul Hasan <hasanrakib455@gmail.com>
2020-08-20 09:20:21 -0700

    Sum of two numbers

---
+++ 4.Working with numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+	int a,b,c,d;
+    float A,B,C,D;
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    d=a-b;
+    printf("%d %d\n",c,d);
+scanf("%f%f",&A,&B);
+    C=A+B;
+    D=A-B;
+    printf("%.1f %.1f",C,D);
+    return 0;
+}
