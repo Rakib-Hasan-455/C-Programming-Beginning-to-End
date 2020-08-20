e2116bd6a40e242e72b8fc77b71f0e17d5cbdb02
Md. Rakibul Hasan <hasanrakib455@gmail.com>
2020-08-20 11:06:12 -0700

    If else simple condition check

---
+++ 0.8.1 If else.c
@@ -0,0 +1,30 @@
+#include <assert.h>
+int main()
+{
+    int input;
+    scanf(" %d",&input);
+    if(input==1){
+        printf("one\n");
+    }else if(input==2){
+        printf("two\n");
+    }else if(input==3){
+        printf("three\n");
+    }else if(input==4){
+        printf("four\n");
+    }else if(input==5){
+        printf("five\n");
+    }else if(input==6){
+        printf("six\n");
+    }else if(input==7){
+        printf("seven\n");
+    }else if(input==8){
+        printf("eight\n");
+    }else if(input==9){
+        printf("nine\n");
+    }
+    else {
+    printf("Greater than 9");
+    }
+
+    return 0;
+}
