1a1a0e0e6dbe5107eab961171550b8a4d5a69180
Md. Rakibul Hasan <hasanrakib455@gmail.com>
2020-08-20 09:23:53 -0700

    Fail or Pass || Input number , print into ternary operator

---
+++ 12.0 Ternary Operator.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+int marks;
+char grade;
+scanf("%d",&marks);
+printf("You have obtained %d marks",marks);
+marks<=50 ? (grade='F' )&&( printf(" and your grade is %c\n",grade)):(grade='P' )&& (printf(" and your grade is %c\n",grade));
+return 0;
+}
