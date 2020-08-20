df213ef85d581bce4f37d2ac471835058780303f
Md. Rakibul Hasan <hasanrakib455@gmail.com>
2020-08-20 09:53:47 -0700

    String printing

---
+++ 0.4.1 Data Type-String.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+char a;
+scanf("%c",&a);
+char b[10];
+scanf("%s",&b);
+char c[10];
+char d[10];
+char e[10];
+scanf("%s%s%s",&c,&d,&e);
+printf("%c\n%s\n%s %s %s",a,b,c,d,e);
+    return 0;
+}
