87d627aa25343156a0dcabdf36289653ad3ceb43
Md. Rakibul Hasan <hasanrakib455@gmail.com>
2020-09-06 07:48:17 -0700

    Drawing R word

---
+++ 0.2 Drawing a Shape.c
@@ -0,0 +1,11 @@
+#incclude<stdio.h>
+int main(){
+printf("######      \n");
+printf("##    ###   \n");
+printf("##       ## \n");
+printf("##   ####   \n");
+printf("## ##       \n");
+printf("##   ###    \n");
+printf("##      ### \n");
+return 0;
+}
