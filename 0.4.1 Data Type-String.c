a512f21a82fc5b91d2217fb3266346371614a64c
Md. Rakibul Hasan <hasanrakib455@gmail.com>
2020-09-06 08:02:35 -0700

    Char and String Printing

--- 0.4.1 Data Type-String.c
+++ 0.4.1 Data Type-String.c
@@ -13,6 +13,6 @@ char c[10];
 char d[10];
 char e[10];
 scanf("%s%s%s",&c,&d,&e);
-printf("%c\n%s\n%s %s %s",a,b,c,d,e);
+printf(" Char is %c\n  Strings are \n %s\n%s %s %s",a,b,c,d,e);
     return 0;
 }
