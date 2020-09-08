3d40157af4573558462da97bceba9e31d833e74d
Md. Rakibul Hasan <hasanrakib455@gmail.com>
2020-09-08 09:51:12 -0700

    row digit triangle pattern

--- 1.2.1.0 For Loop.c
+++ 1.2.1.0 For Loop.c
@@ -4,10 +4,6 @@
     3 3 3
     4 4 4 4
     5 5 5 5 5
-    */
- 
-
-
 #include<stdio.h>
 int main(){
 int n;
@@ -20,4 +16,23 @@ printf(" %d ",row);
 printf(" \n ");
 }
 return 0;
+}
+    input=5
+    5 5 5 5 5
+    4 4 4 4
+    3 3 3
+    2 2
+    1
+    */
+#include<stdio.h>
+int main(){
+	int row,column,input;
+	scanf(" %d",&input);
+	for(row=input;row>=0;row--){
+		for(column=1;column<=row;column++){
+			printf("%d ",row);
+		}
+		printf("\n");
+	}
+	return 0;
 }
