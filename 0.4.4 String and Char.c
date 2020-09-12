1da56473a2db85e44ec64a45cd70542cde71b7cb
Md. Rakibul Hasan <hasanrakib455@gmail.com>
2020-09-12 02:58:35 -0700

    Strcat(add) and Strcpy(replace)

--- 0.4.4 String and Char.c
+++ 0.4.4 String and Char.c
@@ -1,1 +1,20 @@
 
+#include<stdio.h>
+#include<stdlib.h>
+#include<ctype.h>
+#include<string.h>
+
+int main(){
+	char string[100]="Main String \n";
+	strcat(string,"Added Strcat String 1\n");
+	strcat(string,"Added Strcat String 2\n");
+	strcat(string,"Added Strcat String 3\n");
+	strcat(string,"Added Strcat String 4\n");
+	
+	//printf(" %s \n",string);
+	strcpy(string,"Replaced Strcpy String 1\n");
+	//Strcpy removed Main String and all Strcat
+	
+	printf(" %s \n",string);
+	return 0;
+}
