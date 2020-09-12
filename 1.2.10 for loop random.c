8a4dd33f52dc47a45b4ad0c735065624872506a8
Md. Rakibul Hasan <hasanrakib455@gmail.com>
2020-09-12 04:05:25 -0700

    Random number rand()

---
+++ 1.2.10 for loop random.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<ctype.h>
+#include<string.h>
+#include<math.h>
+int main(){
+	int i;
+	while(1){
+		int n;
+		scanf(" %d",&n);
+	for(i=1;i<n;i++){
+	printf("Generated random number= %d \n",rand());
+		}
+		}return 0;
+}
