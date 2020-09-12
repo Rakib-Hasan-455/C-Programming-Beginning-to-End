c2bfebd23f227f56bc385749bfb28c57c0022c93
Md. Rakibul Hasan <hasanrakib455@gmail.com>
2020-09-11 23:18:20 -0700

    Column digit left triangle pattern

--- 1.2.6 for loop.c
+++ 1.2.6 for loop.c
@@ -40,3 +40,43 @@ int main(){
 	}
 	return 0;
 }
+
+/*6
+ 123456
+  12345
+   1234
+    123
+     12
+      1
+
+ ******
+  *****
+   ****
+    ***
+     **
+      *
+      #include<stdio.h>
+int main(){
+	int row,column,input;
+	scanf(" %d",&input);
+	for(row=0;row<=input;row++){
+		for(column=0;column<=row;column++){
+			printf(" ");
+		}
+		for(column=1;column<=input-row;column++){
+			printf("%d",column);
+		}
+		printf("\n");
+	}
+	for(row=0;row<=input;row++){
+		for(column=0;column<=row;column++){
+			printf(" ");
+		}
+		for(column=1;column<=input-row;column++){
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
+*/
