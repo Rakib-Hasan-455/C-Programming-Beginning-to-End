02822c5f7ad9154c03fa9e5f4a7d28ac1a063633
Md. Rakibul Hasan <hasanrakib455@gmail.com>
2020-09-12 02:16:35 -0700

    String under int & isalpha || isdigit use

--- 0.4.3 String & Char.c
+++ 0.4.3 String & Char.c
@@ -1,2 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<ctype.h>
+#include<string.h>
+
+int main(){
+	int Int_Char='r';
+	printf(" %c is a letter\n",Int_Char);
+if(isalpha(Int_Char) ){
+	printf("That latter %c is character\n",Int_Char);
+}
+else{
+	if(isdigit(Int_Char) ){
+		printf("That is %c is a number",Int_Char);
+	}
+	else{
+	printf(" This is not digit or letter");
+}
+}
+
+
+	return 0;
+}
 
  
