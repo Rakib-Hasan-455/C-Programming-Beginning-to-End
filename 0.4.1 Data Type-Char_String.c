fb0b74518f4ad3633fc7b16c8dac62fcaadeac58
Md. Rakibul Hasan <hasanrakib455@gmail.com>
2020-09-14 01:18:58 -0700

    Char && Strings

--- 0.4.1 Data Type-Char_String.c
+++ 0.4.1 Data Type-Char_String.c
@@ -6,13 +6,13 @@
 int main()
 {
 char a;
-scanf("%c",&a);
+scanf("%c",&a); //for char &variable_name can be used
 char b[10];
-scanf("%s",&b);
+scanf("%s",b); //for string only variable name can be used
 char c[10];
 char d[10];
 char e[10];
-scanf("%s%s%s",&c,&d,&e);
+scanf("%s%s%s",c,d,e); //for string only variable name can be used
 printf(" Char is %c\n  Strings are \n %s\n%s %s %s",a,b,c,d,e);
     return 0;
 }
