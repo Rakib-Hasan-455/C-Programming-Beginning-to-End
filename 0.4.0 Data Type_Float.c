8c44130e8e76837b3b723b9bc8bc66396f00341e
Md. Rakibul Hasan <hasanrakib455@gmail.com>
2020-10-19 01:02:07 -0700

    Rounding Float Numbers with floor | ceil

--- 0.4.0 Data Type_Float.c
+++ 0.4.0 Data Type_Float.c
@@ -13,10 +13,18 @@ int main(){
 		printf(" %.2f is floor \n", floor(number2) );
 	//ceil makes it to next highest number or 7.6 as 8
 		printf(" %.2f is ceil\n", ceil(number1) );
-
+	
 	//ceil makes it to next highest number or 7.2 as 8
 		printf(" %.2f is ceil \n", ceil(number2) );
-
+	//mannual ceil
+	int number=10,another_number=3,ceil;
+	if (number % another_number == 0) {
+		ceil = number / another_number;
+	}else{
+		ceil = number / another_number + 1;
+	}	
+	//ANOTHER TECHNIQUE
+		ceil = (number + another_number - 1) / another_number;
 	
 	return 0;
 }
